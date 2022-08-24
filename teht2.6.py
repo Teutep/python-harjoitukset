6699df402fd4db687748f3d38dc7f4dbf268d263
Teuvo <teuvo.almgren@gmail.com>
2022-08-24 14:15:50 +0300

    Tehtäviä

--- teht2.6.py
+++ teht2.6.py
@@ -1,3 +1,13 @@
+# x = random.randint(0, 9)
+# print(f"Kolminumeroinen koodi on: {x}")
+
 import random
-x = random.randint(0, 9)
-print(x)
+Numero = ""
+for x in range(0,3):
+    Numero += str(random.randint(0,9))
+Numero2 = ""
+for x in range(0,4):
+    Numero2 += str(random.randint(1,6))
+
+print(f"Kolminumeroinen luku: {Numero}")
+print(f"Nelinumeroinen luku: {Numero2}")
